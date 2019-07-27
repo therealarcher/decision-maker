34510b8f75203a1ad4936a4da1427cbdc7794224
Simon Garber <Simon.garber@gmail.com>
2019-07-27 18:52:19 -0400

    Added test data for each table in the seeds folder

--- db/schema/02_polls.sql
+++ db/schema/02_polls.sql
@@ -3,12 +3,12 @@
 DROP TABLE IF EXISTS polls CASCADE;
 CREATE TABLE polls (
   id SERIAL PRIMARY KEY NOT NULL,
-  --user_id INTEGER REFERENCES users(id) NOT NULL,
+  user_id INTEGER REFERENCES users(id) NOT NULL,
   title TEXT NOT NULL,
   is_active BOOLEAN DEFAULT TRUE,
   voter_url TEXT NOT NULL,
   admin_url TEXT NOT NULL,
   end_date timestamp,
-  created_at timestamp
-  --winning_option INTEGER REFERENCES options(id)
+  created_at timestamp,
+  winning_option INTEGER REFERENCES options(id)
 );
