c4b52de84706e3bafabe973956aa592244cd8354
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-07-29 23:20:54 +0000

    added email.js script and key info in .env file

--- public/scripts/email.js
+++ public/scripts/email.js
@@ -1,7 +1,10 @@
+require('dotenv').config({ debug: process.env.DEBUG });
+
 const mailgun = require('mailgun-js');
-const DOMAIN = 'sandboxbac46640689e4f21a6b8405d409c6722.mailgun.org';
-const API_KEY = '9f0967f2be3f4082351527cf1c81bdb7-c50f4a19-eba714f5';
-const mg = mailgun({apiKey: API_KEY, domain: DOMAIN});
+
+//const DOMAIN = mg_domain;
+//const API_KEY = mg_api_key;
+const mg = mailgun({apiKey: process.env.API_KEY, domain: process.env.DOMAIN});
 
 const data = {
   from: 'me@samples.mailgun.org',
@@ -9,14 +12,16 @@ const data = {
   subject: 'Thanks for creating your poll!',
   text: `Thank you for creating your poll!  Your administrator link can be found at <admin_url>
   Now it's up to you to send this link <voter_url> to those who you want to include in the voting!
-  Come back to your admin_url page to view the results of the poll.`
+  Come back to your admin_url page anytime to view the results of the poll.`
 };
 
 mg.messages().send(data, (error, body) => {
   console.log(body);
   console.log(error);
-  console.log(data);
+  // console.log(data);
 });
+
+//console.log('process', process.env);
 
 
 // routes.post('/events', (req, res) => {
