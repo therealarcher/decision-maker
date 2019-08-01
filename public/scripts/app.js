daebec24ea2e0cadf5f7d1acc2d59c3894afa719
Sarah Reive <sarahjreive@gmail.com>
2019-08-01 14:23:31 -0400

    modified input fields so that rank value is passed to database upon form submission

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,66 +1,59 @@
 
-let choiceOrder = []
+let choiceOrder = [];
 
 $(document).ready(function() {
-    
-    $('tbody > tr > #draggable').draggable()
-    
-    $("#droppable" ).droppable({
-        drop: function(event,ui) {
-            var droppable = $(this);
-            var draggable = ui.draggable;
-            
-            
-            $(console.log(ui.draggable[0].innerHTML))
-            choiceOrder.push(ui.draggable[0].innerHTML)
-            console.log($(choiceOrder))
-            
-             $('#droppable').empty()
-             $(draggable).draggable({
-                 revert: true
-                 
-            })
-            $(draggable).hide()
-                 
-             
-              
-           for(choice of choiceOrder) {
-               $choice = `<li id="${choice}">${choice}</li>`
-               if(choiceOrder.filter(word => word === choice)[0].toString()!== $(this)){
-                $('#droppable').append($choice)
-               } 
-               $(console.log(ui.draggable[0].innerHTML))
-               $(console.log($($choice).attr("id")))
-                
-           }
-        
-           $("#reset").click(function(e){
-            $(console.log(e))
-            $.each($(draggable), function(){ 
-                if($(this).is(":hidden")) {
-                    $(this).show();
-                }
-                
-                
-             
-            });
-            $(`#droppable`).empty()
-            choiceOrder = [];
-        })
-        
 
-                $(`li`).click( function(e) {
-                    // if(choiceOrder.indexOf($($choice).attr("id")) !== ui.draggable[0].innerHTML) {
-                     $(e.target).remove()   
-                    // }
-                    
-                    $(choiceOrder.pop($(choiceOrder.indexOf($(this).attr( "id" )))))
-                    
-            })
+  $('tbody > tr > #draggable').draggable();
+
+  $("#droppable" ).droppable({
+    drop: function(event,ui) {
+      var droppable = $(this);
+      var draggable = ui.draggable;
+
+
+      $(console.log(ui.draggable[0].innerHTML));
+      choiceOrder.push(ui.draggable[0].innerHTML);
+      console.log($(choiceOrder));
+      //ASSIGN VALUE OF RANK TO INPUT FIELD
+      $(`#${ui.draggable[0].innerHTML}`).attr("value",choiceOrder.length);
+      console.log(choiceOrder.length);
+
+      $('#droppable').empty();
+      $(draggable).draggable({
+        revert: true
+
+      });
+      $(draggable).hide();
+
+
+
+      for (choice of choiceOrder) {
+        $choice = `<li id="${choice}">${choice}</li>`;
+        if (choiceOrder.filter(word => word === choice)[0].toString()!== $(this)){
+          $('#droppable').append($choice);
         }
-     })  
+        $(console.log(ui.draggable[0].innerHTML));
+        $(console.log($($choice).attr("id")));
+
+      }
+
+      $("#reset").click(function(e){
+        $(console.log(e));
+        $.each($(draggable), function(){
+          if ($(this).is(":hidden")) {
+            $(this).show();
+          }
+
+
+
+        });
+        $(`#droppable`).empty();
+        choiceOrder = [];
+      });
+    }
+  });
 });
 
-                     
-            
+
+
 
